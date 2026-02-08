6b48589cf665247ae8d435c682755fbf5edae31d
minefuniogame <elomraniabdelbasset7@gmail.com>
2026-02-08 22:52:15 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -32,8 +32,15 @@ document.addEventListener("DOMContentLoaded", () => {
           </div>
           <div class="game-info">
             <h3>${game.title}</h3>
-           
+            <div class="game-meta">
+            <span>
+              <svg width="14" height="14" viewBox="0 0 24 24" fill="#fbbf24" stroke="#fbbf24" stroke-width="2">
+                <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"/>
+              </svg>
+              ${game.rating}
+            </span>
           </div>
+        </div>
         `;
         grid.appendChild(card);
         }

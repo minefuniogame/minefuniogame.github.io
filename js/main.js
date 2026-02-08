93e14e7e4532b51c74eb6c7cc310c1a7e1de39a9
minefuniogame <elomraniabdelbasset7@gmail.com>
2026-02-08 22:53:42 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -37,7 +37,7 @@ document.addEventListener("DOMContentLoaded", () => {
               <svg width="14" height="14" viewBox="0 0 24 24" fill="#fbbf24" stroke="#fbbf24" stroke-width="2">
                 <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"/>
               </svg>
-              ${game.rating}
+              <p>${game.rating}</p>
             </span>
           </div>
         </div>

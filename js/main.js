1579386b0046fdfa999d47203eadce014ab5971d
minefuniogame <elomraniabdelbasset7@gmail.com>
2026-02-10 20:33:54 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -142,3 +142,34 @@ function addAnalyticsScript() {
   console.log("Ahrefs Analytics loaded ✅");
 };
 addAnalyticsScript()
+
+
+
+function toggleFullScreen() {
+  var iframe = document.getElementById("iframe");
+
+  // Check if any element is currently in fullscreen mode
+  if (!document.fullscreenElement) {
+    // If not, request fullscreen for the iframe
+    if (iframe.requestFullscreen) {
+      iframe.requestFullscreen();
+    } else if (iframe.mozRequestFullScreen) { /* Firefox */
+      iframe.mozRequestFullScreen();
+    } else if (iframe.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+      iframe.webkitRequestFullscreen();
+    } else if (iframe.msRequestFullscreen) { /* IE/Edge */
+      iframe.msRequestFullscreen();
+    }
+  } else {
+    // If an element is already in fullscreen, exit fullscreen
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) { /* Firefox */
+      document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) { /* Chrome, Safari & Opera */
+      document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE/Edge */
+      document.msExitFullscreen();
+    }
+  }
+}

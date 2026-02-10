510ebadc1d68160cc381479f7c205a21ee568eb8
minefuniogame <elomraniabdelbasset7@gmail.com>
2026-02-10 09:55:58 +0100

    Create generate-sitemap.js

---
+++ generate-sitemap.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const path = require("path");
+
+const baseUrl = "https://USERNAME.github.io/REPO";
+
+const gamesDir = path.join(__dirname, "game");
+
+let sitemap = `<?xml version="1.0" encoding="UTF-8"?>\n`;
+sitemap += `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n`;
+
+// homepage
+sitemap += `
+  <url>
+    <loc>${baseUrl}/</loc>
+    <priority>1.0</priority>
+  </url>`;
+
+// games
+fs.readdirSync(gamesDir, { withFileTypes: true })
+  .filter(dir => dir.isDirectory())
+  .forEach(dir => {
+    sitemap += `
+  <url>
+    <loc>${baseUrl}/game/${dir.name}/</loc>
+    <priority>0.8</priority>
+  </url>`;
+  });
+
+sitemap += `\n</urlset>`;
+
+fs.writeFileSync("sitemap.xml", sitemap);

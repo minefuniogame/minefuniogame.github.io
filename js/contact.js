e5d82b5275b733925af242b03f09f82baf7fb6bc
minefuniogame <elomraniabdelbasset7@gmail.com>
2026-02-10 13:15:14 +0100

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -1,13 +1,14 @@
-// Init EmailJS
-(function(){emailjs.init("SJTn_hClPmNz8zF4V");})();
-document.getElementById("contact-form").addEventListener("submit",function(e){
+  // Init EmailJS
+  (function(){emailjs.init("SJTn_hClPmNz8zF4V");})();
+
+  document.getElementById("contact-form").addEventListener("submit", function(e){
     e.preventDefault();
-    emailjs.sendForm("service_a8c7ise","template_gqn2k68",this)
+    emailjs.sendForm("service_a8c7ise","template_fr2y8gb", this)
     .then(function(){
-        document.getElementById("success-msg").style.display="block";
-        document.getElementById("contact-form").reset();
-    },function(error){
-        alert("Error sending message ❌");
-        console.log(error);
+      document.getElementById("success-msg").style.display = "block";
+      document.getElementById("contact-form").reset();
+    }, function(error){
+      alert("Error sending message ❌");
+      console.log(error);
     });
-});
+  });

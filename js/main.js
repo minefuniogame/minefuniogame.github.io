c2ec93ad87d36643f07f2174752e472a87ee84d2
minefuniogame <elomraniabdelbasset7@gmail.com>
2026-02-16 11:29:02 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -68,17 +68,17 @@ document.addEventListener("DOMContentLoaded", () => {
   loadGames(); // call async function
 });
 
-  function addFavIconToPages(){
-  const link = document.createElement('link');
-  link.rel = 'icon';
-  link.href = '/images/minefun-io-favicon.ico';
-  link.type = 'image/x-icon';
+//   function addFavIconToPages(){
+//   const link = document.createElement('link');
+//   link.rel = 'icon';
+//   link.href = '/images/minefun-io-favicon.ico';
+//   link.type = 'image/x-icon';
 
-  // Append it to the head
-  document.head.appendChild(link);
+//   // Append it to the head
+//   document.head.appendChild(link);
 
-  }
-addFavIconToPages()
+//   }
+// addFavIconToPages()
 
 function addAhrefsScript() {
   const script = document.createElement('script');
